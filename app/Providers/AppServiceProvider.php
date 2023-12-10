5d6dc4cd55ed9a9f511cf9328292b6e426443901
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-12-10 17:20:27 +0100

    Add draft.yaml and .blueprint to .gitignore, unguard EloquentModel in AppServiceProvider, and add laravel-shift/blueprint to composer.json

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Blueprint\Contracts\Model;
+use Illuminate\Database\Eloquent\Model as EloquentModel;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        EloquentModel::unguard();
     }
 }
