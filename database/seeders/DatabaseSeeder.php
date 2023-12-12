e2d49ef1b367d08dce3e0242d5494aef55bf00f6
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-12-12 13:57:21 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Attendee;
 use App\Models\Conference;
 use App\Models\Speaker;
+use App\Models\Talk;
 use App\Models\User;
 use App\Models\Venue;
 use Illuminate\Database\Seeder;
@@ -25,19 +27,28 @@ class DatabaseSeeder extends Seeder
             ]);
         }
 
-        $this->command->info('Seeding the database 1');
 
         $venues = Venue::factory(50)->create();
-        $this->command->info('Seeding the database2');
-        $conference = Conference::factory(100)
-            ->recycle($venues)
-            ->create();
-        $this->command->info('Seeding the database 3');
+
+        $talk = Talk::factory(500)->create();
+
         $speaker = Speaker::factory(500)
             ->withTalks(3)
-            ->recycle($conference)
+            ->recycle(
+                Conference::factory(10)
+                    ->recycle($venues)
+                    ->create()
+            )
             ->create();
-        $this->command->info('Seeding the database 4');
 
+        $conference = Conference::factory(20)
+            ->recycle($venues)
+            ->hasAttached($speaker)
+            ->hasAttached($talk)
+            ->create();
+
+        $attendee = Attendee::factory(200)
+            ->forConference($conference->random()->first())
+            ->create();
     }
 }
